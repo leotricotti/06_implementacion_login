102947c78b0ad6dcec36e2adfd8b0206715d958e
Leonardo Tricotti <tricottileo@gmail.com>
2023-08-20 21:00:43 -0300

    commit

--- src/dao/dbmanager/carts.manager.js
+++ src/dao/dbmanager/carts.manager.js
@@ -3,32 +3,67 @@ import cartsModel from "../models/carts.model.js";
 export default class Cart {
   // Método asyncrono para obtener todos los carritos
   getAll = async () => {
-    let result = await cartsModel.find().lean();
-    return result;
+    try {
+      let result = await cartsModel.find().lean();
+      return result;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
   };
+
   // Método asyncrono para obtener un carrito
   getOne = async (id) => {
-    let result = await cartsModel.findById(id).lean();
-    return result;
+    try {
+      let result = await cartsModel.findById(id).lean();
+      return result;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
   };
+
   // Método asyncrono para crear un carrito
   saveCart = async (cart) => {
-    let result = await cartsModel.create(cart);
-    return result;
+    try {
+      let result = await cartsModel.create(cart);
+      return result;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
   };
+
   // Método asyncrono para eliminar un producto del carrito
   updateCart = async (id, cart) => {
-    let result = await cartsModel.updateOne({ _id: id }, cart);
-    return result;
+    try {
+      let result = await cartsModel.updateOne({ _id: id }, cart);
+      return result;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
   };
+
   // Método asyncrono para vaciar el carrito
   emptyCart = async (id, cart) => {
-    let result = await cartsModel.updateOne({ _id: id }, cart);
-    return result;
+    try {
+      let result = await cartsModel.updateOne({ _id: id }, cart);
+      return result;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
   };
+
   // Método asyncrono para popular el carrito
   populatedCart = async (id) => {
-    let result = await cartsModel.findById(id).populate("products.product");
-    return result;
+    try {
+      let result = await cartsModel.findById(id).populate("products.product");
+      return result;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
   };
 }
