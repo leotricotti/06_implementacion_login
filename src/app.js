206b7c5e958f0ac9dc11887d229b263d229cfb4e
Leonardo Tricotti <tricottileo@gmail.com>
2023-08-22 18:14:59 -0300

    commit

--- src/app.js
+++ src/app.js
@@ -125,6 +125,7 @@ io.on("connection", async (socket) => {
 
   // Obtener datos de la base de datos
   socket.on("nextPage", async (page) => {
+    console.log("El cliente solicita la página: ", page);
     try {
       const products = await productsManager.getAll();
       const orderedProducts = products.reverse();
